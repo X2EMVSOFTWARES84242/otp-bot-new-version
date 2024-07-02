589a76fc134ec13ba987b5fb13273c7e1637d48e
X2EMVSOFTWARES84242 <ghgddd26444@gmail.com>
2024-07-01 21:42:59 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,2 +1,3 @@
 Updating new version 
-
+https://discord.com/oauth2/authorize?client_id=1257464027769077830&permissions=1759218604441591&integration_type=0&scope=bot+applications.commands
+1228856561485742152
